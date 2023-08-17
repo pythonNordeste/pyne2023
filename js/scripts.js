fa6d19f6fe4b4698326a610b0619dc746f3e554d
Juliana Barros Lima <judiembl@gmail.com>
2023-08-17 00:41:56 -0300

    fix: fixing the position

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  let x = document.getElementById("navbar");
-  if (x.className === "navbar") {
-    x.className += "responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
